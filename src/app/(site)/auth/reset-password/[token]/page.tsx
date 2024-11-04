66a8de231f7fce1a4fa06e8e0cc80f3dd7e45c89
Steven <lopesteven@gmail.com>
2024-11-04 10:00:14 -0600

    Fix: Removed unused components and updated imports

--- src/app/(site)/auth/reset-password/[token]/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ResetPassword from "@/components/Auth/ResetPassword";
-
-const ResetPasswordPage = ({ params }: { params: { token: string } }) => {
-  return <ResetPassword token={params.token} />;
-};
-
-export default ResetPasswordPage;
