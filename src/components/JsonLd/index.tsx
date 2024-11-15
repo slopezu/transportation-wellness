1d3814ce43dc0cbcb06f71601448d47d42e41a77
Steven López Umaña <100870332+slopezu@users.noreply.github.com>
2024-11-14 22:20:56 -0600

    Update index.tsx
    updating JsonLD para asociar google maps

--- src/components/JsonLd/index.tsx
+++ src/components/JsonLd/index.tsx
@@ -1,14 +1,66 @@
-import React from 'react'
+export default function JsonLd() {
+  const jsonLdData = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "Wellness Transportation - Shuttle Service",
+    "image": "https://transportation-wellness.com/logo.png",
+    "description": "Easy online booking website dedicated to the private shuttle transportation from San José, Guanacaste, La Fortuna and anywhere Costa Rica.",
+    "@id": "https://transportation-wellness.com",
+    "url": "https://transportation-wellness.com",
+    "telephone": "+506 89680765",
+    "email": "reservations@transportation-wellness.com",
+    "priceRange": "$$",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "Liberia, Guanacaste",
+      "addressLocality": "Liberia",
+      "addressRegion": "Guanacaste",
+      "postalCode": "50101",
+      "addressCountry": "CR"
+    },
+    "geo": {
+      "@type": "GeoCoordinates",
+      "latitude": 10.6326807,
+      "longitude": -85.4404804
+    },
+    "areaServed": {
+      "@type": "GeoCircle",
+      "geoMidpoint": {
+        "@type": "GeoCoordinates",
+        "latitude": 10.6326807,
+        "longitude": -85.4404804
+      },
+      "geoRadius": "300000"
+    },
+    "serviceArea": {
+      "@type": "GeoCircle",
+      "geoMidpoint": {
+        "@type": "GeoCoordinates",
+        "latitude": 10.6326807,
+        "longitude": -85.4404804
+      },
+      "geoRadius": "300000"
+    },
+    "openingHoursSpecification": {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ],
+      "opens": "00:00",
+      "closes": "23:59"
+    }
+  };
 
-interface JsonLdProps {
-  data: Record<string, any>
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdData) }}
+    />
+  );
 }
-
-const JsonLd: React.FC<JsonLdProps> = ({ data }) => (
-  <script
-    type="application/ld+json"
-    dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-  />
-)
-
-export default JsonLd
