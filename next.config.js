2239689ed89af9559002a8eaaebcd87bd6f4e0b4
Steven López Umaña <100870332+slopezu@users.noreply.github.com>
2024-11-18 14:08:59 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,21 +3,50 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "transportation-wellness.com"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
+        hostname: "*.public.blob.vercel-storage.com",
       },
     ],
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  env: {
-    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'transportation-wellness.vercel.app',
+          },
+        ],
+        destination: 'https://transportation-wellness.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/_error',
+        has: [
+          {
+            type: 'query',
+            key: 'code',
+            value: '404',
+          },
+        ],
+      },
+    ];
   },
 };
 
