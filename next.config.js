24505dbd817849d663dde11ffa6298a3d5eb1c25
Steven López Umaña <100870332+slopezu@users.noreply.github.com>
2024-11-18 14:29:39 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["localhost", "transportation-wellness.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  env: {
-    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  },
   async redirects() {
     return [
       {
@@ -32,12 +13,8 @@ const nextConfig = {
         destination: 'https://transportation-wellness.com/:path*',
         permanent: true,
       },
-    ];
+    ]
   },
-  i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'en',
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
